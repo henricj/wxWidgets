39e4da777daf90813a42d81061f96184e932a42a
Henric Jungheim <software@henric.org>
2017-06-14 22:40:32 -0700

    Apply accessibility.diff

--- include/wx/msw/setup0.h
+++ include/wx/msw/setup0.h
@@ -65,11 +65,11 @@
 // NDEBUG) or, on the contrary, enable more asserts, including the usually
 // disabled ones, in the debug build (then do it inside #ifndef NDEBUG)
 //
-// #ifdef NDEBUG
-//  #define wxDEBUG_LEVEL 0
-// #else
-//  #define wxDEBUG_LEVEL 2
-// #endif
+#ifdef NDEBUG
+ #define wxDEBUG_LEVEL 0
+#else
+ #define wxDEBUG_LEVEL 1
+#endif
 
 // wxHandleFatalExceptions() may be used to catch the program faults at run
 // time and, instead of terminating the program with a usual GPF message box,
@@ -1377,7 +1377,7 @@
 // Default is 0.
 //
 // Recommended setting (at present): 0
-#define wxUSE_ACCESSIBILITY 0
+#define wxUSE_ACCESSIBILITY 1
 
 // ----------------------------------------------------------------------------
 // miscellaneous settings

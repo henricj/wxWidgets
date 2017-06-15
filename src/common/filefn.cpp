f9a09779670da11af275dc66cda6a02f61372423
Henric Jungheim <software@henric.org>
2017-06-14 22:31:28 -0700

    Remove unused variable from fix_rename.diff

--- src/common/filefn.cpp
+++ src/common/filefn.cpp
@@ -1191,7 +1191,6 @@ wxRenameFile(const wxString& file1, const wxString& file2, bool overwrite)
     // For explanation, see:  (warning...based mostly on observed behavior)
     //   http://bugzilla.audacityteam.org/show_bug.cgi?id=1266
     //   https://github.com/audacity/audacity/pull/94
-  unsigned long doserrno = 0;
   for (int i = 0; i < 2000; i++)
   {
     if ( wxRename (file1, file2) == 0 )
